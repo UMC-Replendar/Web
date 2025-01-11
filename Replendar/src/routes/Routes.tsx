4f1a20b06c9ff510529b705fca84278c57a81693
Seonyeong Park <bagseonyeong452@gmail.com>
2025-01-11 21:55:39 +0900

    first community

--- Replendar/src/routes/Routes.tsx
+++ Replendar/src/routes/Routes.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Info from '../pages/Info';
+import Community from '../pages/community';
+import Login from '../pages/login';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>}></Route>
       <Route path="/info" element={<Info />}></Route>
+      <Route path="/community" element={<Community />}></Route>
+      <Route path="/login" element={<Login />}></Route>
     </Routes>
   );
 }
