50b5a8bce7d42d01065353bddd91ebd6ea40479d
Seonyeong Park <bagseonyeong452@gmail.com>
2025-01-19 15:51:51 +0900

    second commit

--- Replendar/src/pages/community.tsx
+++ Replendar/src/pages/community.tsx
@@ -1,104 +1,194 @@
-import { useState } from 'react';
 import styled from 'styled-components';
+import MoreIcon from '../components/CommuComponents/MoreIcon';
+import { useState } from 'react';
+import CloseIcon from '../components/CommuComponents/CloseIcon';
 
-const Container = styled.div`
-  background-color: lightgray;
-  display: flex;
-`;
-const TextBtn = styled.button`
-  background: none;
-  border: none;
-  width: 100px;
-  cursor: pointer;
-  &:focus {
-    text-decoration: underline;
+const StyledDiv = styled.div<{ expanded: boolean }>`
+  background: rgba(252, 246, 245, 1);
+  width: 100%;
+  height: ${(props) => (props.expanded ? '500px' : '250px')};
+  border-radius: 20px;
+  box-shadow: 0px 5px 15.7px 0px rgba(177, 198, 187, 0.4);
+  margin-bottom: 10px;
+  @media (max-width: 1200px) {
+    width: 100%; /* 화면이 작아지면 100%로 변경 */
+  }
+  @media (max-width: 1200px) {
+    width: 100%; /* 개별 요소가 화면 크기에 따라 변화 */
+  }
+
+  @media (max-width: 480px) {
+    width: 80%; /* 모바일에서는 80% */
   }
 `;
-const TextBtnDiv = styled.div`
-  position: relative;
-  left: 50px;
-  display: flex;
-  flex-direction: column;
-  width: 300px;
+
+const CustomBtn = styled.button`
+  background: rgba(243, 243, 243, 1);
+  width: 100px;
+  height: 30px;
+  border-radius: 50px;\
+border:none;
+font-size:10px;
+padding:0px;
 `;
 
-const ContentDiv = styled.div`
-  background-color: white;
-`;
-const SpaceBetDiv = styled.div`
+const BtnContainer = styled.div`
   display: flex;
-  justify-content: space-between;
-  text-align: end;
+  gap: 8px;
 `;
 
-const ContentContainer = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  gap: 10px;
   width: 100%;
   height: 100%;
+  padding: 0 50px;
+`;
+
+const Gaph2 = styled.h2`
+  padding: 10px 0px;
+  margin: 0px;
+`;
+const SpaceBtwDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Community = () => {
-  const [activeMenu, setActiveMenu] = useState('news'); // 기본 메뉴는 '친구 소식'
+  const [menuState, setMenuState] = useState({
+    activeMenu: '친구등록',
+    expanded: false,
+  });
 
-  const renderContent = () => {
-    switch (activeMenu) {
-      case 'news':
-        return (
-          <ContentContainer>
-            <SpaceBetDiv>
-              <h3>친구 소식</h3>
-              <TextBtn>더보기</TextBtn>
-            </SpaceBetDiv>
-            <ContentDiv>친구 소식 content</ContentDiv>
-          </ContentContainer>
-        );
-      case 'list':
-        return (
-          <ContentContainer>
-            {' '}
-            <SpaceBetDiv>
-              <h3>친구 목록</h3>
-              <TextBtn>더보기</TextBtn>
-            </SpaceBetDiv>
-            <ContentDiv>친구 목록 content</ContentDiv>
-          </ContentContainer>
-        );
-      case 'add':
-        return (
-          <ContentContainer>
-            <SpaceBetDiv>
-              <h3>친구 등록</h3>
-              <TextBtn>더보기</TextBtn>
-            </SpaceBetDiv>
-            <ContentDiv>친구 등록 content</ContentDiv>
-          </ContentContainer>
-        );
-      case 'manage':
-        return (
-          <ContentContainer>
-            <SpaceBetDiv>
-              <h3>친구 관리</h3>
-              <TextBtn>더보기</TextBtn>
-            </SpaceBetDiv>
-            <ContentDiv className="contentDiv">친구 관리 content</ContentDiv>
-          </ContentContainer>
-        );
-    }
+  const handleMenuClick = (menu: string) => {
+    setMenuState({ activeMenu: menu, expanded: false });
   };
 
+  const handleExpandClick = () => {
+    setMenuState((prevState) => ({
+      ...prevState,
+      expanded: !prevState.expanded,
+    }));
+    console.log(menuState);
+  };
+
+  //임시데이터
+  const data1 = [
+    '친구 등록 내용 1',
+    '친구 등록 내용 2',
+    '친구 등록 내용 3',
+    '친구 등록 내용 4',
+    '친구 등록 내용 5',
+    '친구 등록 내용 6',
+    '친구 등록 내용 7',
+    '친구 등록 내용 8',
+    '친구 등록 내용 9',
+    '친구 등록 내용 10',
+  ];
+  const data2 = [
+    '친구 목록 내용 1',
+    '친구 목록 내용 2',
+    '친구 목록 내용 3',
+    '친구 목록 내용 4',
+    '친구 목록 내용 5',
+    '친구 목록 내용 6',
+    '친구 등록 내용 7',
+    '친구 등록 내용 8',
+    '친구 등록 내용 9',
+    '친구 등록 내용 10',
+  ];
+  const data3 = [
+    '친data3 1',
+    '친구 목록 내용 2',
+    '친구 목록 내용 3',
+    '친구 목록 내용 4',
+    '친구 목록 내용 5',
+    '친구 목록 내용 6',
+    '친구 등록 내용 7',
+    '친구 등록 내용 8',
+    '친구 등록 내용 9',
+    '친구 등록 내용 10',
+  ];
+  const data4 = [
+    ' ㅇㅁㅅㅁ ㅇㅁdaata3 1',
+    '친구 목록 내용 2',
+    '친구 목록 내용 3',
+    '친구 목록 내용 4',
+    '친구 목록 내용 5',
+    '친구 목록 내용 6',
+    '친구 등록 내용 7',
+    '친구 등록 내용 8',
+    '친구 등록 내용 9',
+    '친구 등록 내용 10',
+  ];
+
+  interface MenuContentProps {
+    expanded: boolean;
+    data: string[];
+  }
+
+  const MenuContent: React.FC<MenuContentProps> = ({ expanded, data }) => {
+    return expanded
+      ? data.map((item, index) => <div key={index}>{item}</div>)
+      : data.slice(0, 3).map((item, index) => <div key={index}>{item}</div>);
+  };
+  let dataToShow;
+  switch (menuState.activeMenu) {
+    case '친구등록':
+      dataToShow = data1;
+      break;
+    case '친구목록':
+      dataToShow = data2;
+      break;
+    case '친구소식':
+      dataToShow = data3;
+      break;
+    case '친구관리':
+      dataToShow = data4;
+      break;
+    default:
+      dataToShow = data1;
+  }
   return (
     <Container>
-      <TextBtnDiv>
-        <h2>친구</h2>
-        <TextBtn onClick={() => setActiveMenu('news')}>친구 소식</TextBtn>
-        <TextBtn onClick={() => setActiveMenu('list')}>친구 목록</TextBtn>
-        <TextBtn onClick={() => setActiveMenu('add')}>친구 등록</TextBtn>
-        <TextBtn onClick={() => setActiveMenu('manage')}>친구 관리</TextBtn>
-      </TextBtnDiv>
+      <Gaph2>친구</Gaph2>
+      <SpaceBtwDiv>
+        <BtnContainer>
+          <CustomBtn onClick={() => handleMenuClick('친구등록')}>
+            친구 등록
+          </CustomBtn>
+          <CustomBtn onClick={() => handleMenuClick('친구목록')}>
+            친구 목록
+          </CustomBtn>
+          <CustomBtn onClick={() => handleMenuClick('친구소식')}>
+            친구 소식
+          </CustomBtn>
+          <CustomBtn onClick={() => handleMenuClick('친구관리')}>
+            친구 관리
+          </CustomBtn>
+        </BtnContainer>
 
-      {renderContent()}
+        {menuState.expanded ? (
+          <CloseIcon onClick={handleExpandClick} />
+        ) : (
+          <MoreIcon onClick={handleExpandClick} />
+        )}
+      </SpaceBtwDiv>
+      <StyledDiv expanded={menuState.expanded}>
+        <MenuContent expanded={menuState.expanded} data={dataToShow} />
+      </StyledDiv>
+      <Gaph2>학과</Gaph2>
+      <SpaceBtwDiv>
+        <BtnContainer>
+          <CustomBtn>학과 소식</CustomBtn>
+          <CustomBtn>친구 목록</CustomBtn>
+          <CustomBtn>친구 소식</CustomBtn>
+          <CustomBtn>친구 관리</CustomBtn>
+        </BtnContainer>
+        <MoreIcon></MoreIcon>
+      </SpaceBtwDiv>
+      <StyledDiv expanded={menuState.expanded}></StyledDiv>
     </Container>
   );
 };
