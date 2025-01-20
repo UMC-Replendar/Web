6226962cbc5f2d9de1bd8dfcbbab11511d2da9d4
coldgeon <cksrjs4726@gmail.com>
2025-01-20 18:04:36 +0900

    [Style] setting page 스타일링

--- Replendar/src/pages/Settings.tsx
+++ Replendar/src/pages/Settings.tsx
@@ -6,67 +6,120 @@ function Settings() {
   const userId = '닉네임';
 
   return (
-    <SettingsContainer>
-      <SectionContainer>
+    <SettingsWrapper>
+      <TitleContainer>
         <SettingsIcon src={Setting} />
         <SettingsTitle>환경설정</SettingsTitle>
-      </SectionContainer>
-      <SectionContainer>
-        <SectionTitle>계정</SectionTitle>
-        <SectionLink>아이디 {userId}</SectionLink>
-      </SectionContainer>
-      <SectionContainer>
-        <SectionTitle>앱 설정</SectionTitle>
-        <SectionLink>
-          <Link to={'/settings/theme'}>테마 설정</Link>
-        </SectionLink>
-      </SectionContainer>
-      <SectionContainer>
-        <SectionTitle>이용 안내</SectionTitle>
-        <Link to={'/settings/ask'}>
-          <SectionLink>문의하기</SectionLink>
-        </Link>
-        <Link to={'/settings/notify'}>
-          <SectionLink>공지사항</SectionLink>
-        </Link>
-        <Link to={'/settings/detail'}>
-          <SectionLink>서비스 이용약관</SectionLink>
-        </Link>
-      </SectionContainer>
-      <SectionContainer>
-        <SectionTitle>기타</SectionTitle>
-        <SectionLink>회원 탈퇴</SectionLink>
-        <SectionLink>로그아웃</SectionLink>
-      </SectionContainer>
-    </SettingsContainer>
+      </TitleContainer>
+      <ComponentContainer>
+        <SectionContainer>
+          <SectionTitle>계정</SectionTitle>
+          <SectionLink>
+            <span>아이디</span>
+            <NicknameContainer> {userId}</NicknameContainer>
+          </SectionLink>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>앱 설정</SectionTitle>
+          <SectionLink>
+            <Link to={'/settings/theme'}>테마 설정</Link>
+          </SectionLink>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>이용 안내</SectionTitle>
+          <Link to={'/settings/ask'}>
+            <SectionLink>문의하기</SectionLink>
+          </Link>
+          <Link to={'/settings/notify'}>
+            <SectionLink>공지사항</SectionLink>
+          </Link>
+          <Link to={'/settings/detail'}>
+            <SectionLink>서비스 이용약관</SectionLink>
+          </Link>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>기타</SectionTitle>
+          <SectionLink>회원 탈퇴</SectionLink>
+          <SectionLink>로그아웃</SectionLink>
+        </SectionContainer>
+      </ComponentContainer>
+    </SettingsWrapper>
   );
 }
 
 export default Settings;
 
-const SettingsContainer = styled.div`
-  display: flex;
-  width: 100%;
-  height: 130%;
+const SettingsWrapper = styled.div`
   flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 50px;
+  margin-top: 70px;
+  /* width: 100%; */
   gap: 20px;
 `;
 
-const SettingsIcon = styled.img``;
+const ComponentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  gap: 60px;
+  align-items: flex-start;
+  padding: 40px 163px 63px 39px;
+  border-radius: 20px;
+  background: #fcf6f5;
 
-const SettingsTitle = styled.span``;
-
-const SectionContainer = styled.div`
-  &:first-child {
-    display: flex;
-    gap: 10px;
-    align-items: center;
-  }
+  box-shadow: 0px 7px 22.3px -6px #cdcdcd;
 `;
+
+const SettingsIcon = styled.img`
+  width: 30px;
+  height: 30px;
+`;
+
+const SettingsTitle = styled.span`
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 140%;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+`;
+
+const SectionContainer = styled.div``;
 
 const SectionTitle = styled.span`
-  font-size: 20px;
-  font-weight: bold;
+  color: var(--, #000);
+
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 140%;
 `;
 
-const SectionLink = styled.div``;
+const SectionLink = styled.div`
+  color: var(--, #666);
+
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%;
+`;
+
+const NicknameContainer = styled.span`
+  color: rgba(102, 102, 102, 0.7);
+
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 140%;
+`;
