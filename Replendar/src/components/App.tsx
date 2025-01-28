30c9eea8b6aca82b373b9261dece79c7ecde75e2
coldgeon <cksrjs4726@gmail.com>
2025-01-29 00:59:17 +0900

    [Deploy]하하

--- Replendar/src/components/App.tsx
+++ Replendar/src/components/App.tsx
@@ -4,7 +4,7 @@ import Router from '../routes/Routes';
 import Sidebar from './Sidebar';
 import NavBar from './Navbar';
 import { useLocation } from 'react-router-dom';
-// import OngoingTasks from './OngoingTasks';
+import Modal from '../modal/Modal';
 
 function App() {
   const location = useLocation();
@@ -14,6 +14,7 @@ function App() {
 
   return (
     <PageWrapper>
+      <Modal />
       {!hideLayout && <Sidebar />}
 
       <MainWrapper>
