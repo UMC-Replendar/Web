b868c1e4758ab0c16eb25bbd8b9a5a9dddcf9ca6
todotoday <s260822@naver.com>
2025-01-29 20:06:34 +0900

    [FEAT] 메인_과제수정 모달 완료

--- Replendar/src/main.tsx
+++ Replendar/src/main.tsx
@@ -9,7 +9,7 @@ import { GlobalStyle } from './assets/css/GlobalStyle.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
