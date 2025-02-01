77781261dae99578a355506adc2dc51fdaa9c084
coldgeon <cksrjs4726@gmail.com>
2025-02-01 16:51:10 +0900

    [Feat]login 로직 생성(버그 해결 전)

--- Replendar/src/components/LoginComponents/Redirect.tsx
+++ Replendar/src/components/LoginComponents/Redirect.tsx
@@ -1,8 +1,31 @@
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 export default function Redirect() {
+  const navigate = useNavigate();
   const AUTHORIZE_CODE: string | null = new URLSearchParams(
     window.location.search
   ).get('code');
+  axios
+    .get(`${import.meta.env.VITE_BACKEND_API_URL}/code=${AUTHORIZE_CODE}`, {})
+    .then((response) => {
+      console.log(response);
 
-  console.log(AUTHORIZE_CODE);
-  return <h1>리다이렉션 페이지</h1>;
+      //넘어오는 jwt 토큰을 받음
+      const JWT_TOKEN: any = response;
+
+      localStorage.setItem('token', JWT_TOKEN);
+      alert('로그인에 성공했습니다');
+      if (response.data.userID == null) {
+        navigate('/signup');
+      } else {
+        navigate('/');
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      alert('로그인에 실패했습니다.');
+      navigate('/login');
+    });
+  return <h1>리다이렉트 중입니다.</h1>;
 }
