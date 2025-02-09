42d0e6a97a3c0803ae495c6a5afcfc96a7b89459
Seonyeong Park <bagseonyeong452@gmail.com>
2025-02-10 01:44:08 +0900

    한글자수정

--- Replendar/src/hooks/useGetInfiniteData.ts
+++ Replendar/src/hooks/useGetInfiniteData.ts
@@ -1,7 +1,7 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { axiosInstance } from '../apis/axios-instance';
 
-function useGetInfiniteData(url: string, size: number = 5) {
+function useGetInfiniteData(url: string, size: number = 10) {
   const getData = async ({ pageParam = 1 }: { pageParam: number }) => {
     const { data } = await axiosInstance.get(
       `${url}?page=${pageParam}&size=${size}`
