b964ac7104712dc58f9362c16ffdd09375518c4d
coldgeon <cksrjs4726@gmail.com>
2025-02-10 20:22:49 +0900

    []

--- Replendar/src/components/infoComopnents/taskPage/Fakelogin.tsx
+++ Replendar/src/components/infoComopnents/taskPage/Fakelogin.tsx
@@ -81,7 +81,6 @@ const Fakelogin = () => {
 
       if (data.isSuccess && data.result.accessToken) {
         const { accessToken, email, id, nickname, theme } = data.result;
-        // localStorage.setItem('token', data.result.accessToken); // ✅ JWT 저장
         console.log(data);
         setAuth(accessToken, email, id, nickname, theme); // ✅ Zustand에 토큰 저장
         if (nickname == null) {
